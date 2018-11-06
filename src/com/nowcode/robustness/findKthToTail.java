e61fc171858afbffe4befe778f7b895b3e61e8ad
LuJian <863956237@qq.com>
2018-11-06 15:37:00 +0800

    finish FindKthToTail problem

--- src/com/nowcode/robustness/findKthToTail.java
+++ src/com/nowcode/robustness/findKthToTail.java
@@ -20,7 +20,22 @@ class ListNode {
 
 class Solution3 {
     public ListNode FindKthToTail(ListNode head,int k) {
-    	
+    	if(k<1)
+    		return null;
+    	int len = 0;
+    	ListNode p = head;
+    	while(p!=null) {
+    		len++;
+    		p = p.next;
+    	}
+    	if(k>len)
+    		return null;
+    	int index = len - k;
+    	p = head;
+    	for(int i=0;i<index;i++) {
+    		p = p.next;
+    	}
+    	return p;
     }
 }
  
