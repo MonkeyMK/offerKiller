a31a972b6f5361e3b8e2db59d1e55342ccde4cf7
LuJian <863956237@qq.com>
2018-11-06 11:05:40 +0800

    finish arrayOrder problem

--- src/com/nowcode/robustness/arrayOrder.java
+++ src/com/nowcode/robustness/arrayOrder.java
@@ -11,10 +11,20 @@ public class arrayOrder {
 class Solution1 {
     public void reOrderArray(int [] array) {
         int len = array.length;
-        int[] even = new int[len];
-        int oddIndex = 0;
+        int[] orderArray = new int[len];
+        int j = 0;
         for(int i=0;i<len;i++) {
-        	if((array[i] & 1))
+        	if((array[i] & 1) == 1) {
+        		orderArray[j++] = array[i];
+        	}
+        }
+        for(int i=0;i<len;i++) {
+        	if((array[i] & 1) == 0) {
+        		orderArray[j++] = array[i];
+        	}
+        }
+        for(int i=0;i<len;i++) {
+        	array[i] = orderArray[i];
         }
     }
 }
---
+++ src/com/nowcode/robustness/findKthToTail.java
@@ -0,0 +1,26 @@
+package com.nowcode.robustness; 
+/** 
+* @author 小炉子 863956237@qq.com: 
+* @version 创建时间：2018年11月6日 上午10:58:08 
+* 类说明 
+*/
+public class findKthToTail {
+
+}
+
+
+class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+class Solution3 {
+    public ListNode FindKthToTail(ListNode head,int k) {
+    	
+    }
+}
+ 
